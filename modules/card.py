3250fdd864a54209b1f5ef3ecaaa72ebe17bf534
Chris <cjgarcia1984@gmail.com>
2023-12-23 19:55:01 -0800

    face names

--- modules/card.py
+++ modules/card.py
@@ -1,5 +1,6 @@
 class Card(object):
     SUIT_SYMBOLS = {"Hearts": "♥", "Diamonds": "♦", "Clubs": "♣", "Spades": "♠"}
+    SPECIAL_VALUES = {1: "A", 11: "J", 12: "Q", 13: "K"}
 
     def __init__(self, suit, number):
         self.suit = suit
@@ -11,8 +12,12 @@ class Card(object):
         color_code = "\033[91m" if self.color == "red" else ""
         reset_code = "\033[0m" if self.color == "red" else ""
         suit_symbol = Card.SUIT_SYMBOLS.get(self.suit, "")
+
+        # Use special value names for 1, 11, 12, and 13
+        number_or_face = Card.SPECIAL_VALUES.get(self.number, self.number)
+
         return (
-            f"{color_code}{self.number}{suit_symbol}{reset_code}"
+            f"{color_code}{number_or_face}{suit_symbol}{reset_code}"
             if self.visible
             else "[Hidden]"
         )
