dd24ef3e92ded2381b429ea04d32b6522a7f488b
Chris <cjgarcia1984@gmail.com>
2023-12-29 22:00:39 -0800

    adapted for refactored Solitaire

--- dqn.py
+++ dqn.py
@@ -2,37 +2,37 @@ from stable_baselines3 import DQN
 from stable_baselines3.dqn.policies import MlpPolicy
 from stable_baselines3.common.env_checker import check_env
 from stable_baselines3.common.evaluation import evaluate_policy
-from modules.callback import ExplorationRateCallback
-from modules.solitaire_env import SolitaireEnv  # Replace with the path to your SolitaireEnv
+# Replace with the path to your SolitaireEnv
+from modules.solitaire_env import SolitaireEnv
+from modules.callback import ModelDataCallback
 import yaml
 # Create the environment
-env = SolitaireEnv(config=yaml.safe_load(open("/home/chris/Solitaire/configs/config.yaml")))
+env = SolitaireEnv(config=yaml.safe_load(
+    open("/home/chris/Solitaire/configs/config.yaml")))
 
 # Check the environment (optional but recommended)
 check_env(env, warn=True)
 
+
 def train_dqn_agent():
     # Initialize the agent
-    model = DQN(MlpPolicy, env, verbose=1, 
-                learning_rate=0.001,
-                batch_size=32,
-                learning_starts=5000,
-                buffer_size=1000000,
-                exploration_fraction=0.5, 
-                exploration_final_eps=0.01, 
+    model = DQN(MlpPolicy, env, verbose=1,
+                learning_rate=0.000001,
+                batch_size=16,
+                learning_starts=50000,
+                buffer_size=5000000,
+                exploration_fraction=0.75,
+                exploration_final_eps=0.01,
                 target_update_interval=1000,
                 )
 
     # Create the callback and pass the model to it
-    callback = ExplorationRateCallback(model=model)
+    callback = ModelDataCallback(model=model)
 
     # Train the agent
     print("Training the DQN agent...")
     model.learn(total_timesteps=1000000, callback=callback)
     print("Training completed!")
-
-    # Save the model (optional)
-    model.save("dqn_solitaire")
 
     return model
 
@@ -50,6 +50,7 @@ def test_dqn_agent(model):
         env.render()
         if terminated:
             obs, info = env.reset()
+
 
 if __name__ == "__main__":
     model = train_dqn_agent()
--- modules/solitaire_env.py
+++ modules/solitaire_env.py
@@ -5,11 +5,13 @@ import numpy as np
 import random
 import csv
 
+
 def sequential_num_generator(start=1, end=None):
     i = start
     while end is None or i < end:
         yield i
         i += 1
+
 
 # Constants for encoding
 NUM_SUITS = 4
@@ -27,21 +29,23 @@ class SolitaireEnv(gymnasium.Env):
     def __init__(self, config=None):
         super(SolitaireEnv, self).__init__()
         # Initialize the Solitaire game
-        self.config = config.get('env')
+        self.config = config
         self.game = Solitaire(config=config)
-        self.action_log_file = self.config.get('action_log_file', 'action_log.csv')
+        self.action_log_file = self.config.get("env").get(
+            'action_log_file', 'action_log.csv')
         with open(self.action_log_file, 'w', newline='') as file:
             writer = csv.writer(file)
-            writer.writerow(['total_episode_count', 'episode', 'step', 'action', 'source_stack', 'destination_stack', 'num_cards', 'reward', 'terminated', 'exploration_rate'])
+            writer.writerow(['total_episode_count', 'episode', 'step', 'action', 'source_stack',
+                            'destination_stack', 'num_cards', 'reward', 'terminated', 'exploration_rate'])
 
         self.num_source_stacks = 10
         self.num_destinations = 8  # 7 tableau stacks, 1 foundation stack
-        self.max_cards_per_move = NUM_RANKS  # Maximum number of cards that can be moved at once
+        # Maximum number of cards that can be moved at once
+        self.max_cards_per_move = NUM_RANKS
         # Define action space (source_stack, destination_stack, num_cards)
         # 9 source stacks, 8 destination stacks, 1 card
         self.action_space = spaces.Discrete(
             self.num_source_stacks * self.num_destinations * self.max_cards_per_move)
-        self.global_action_history = []
         self.action_repeat_threshold = 2
 
         # Number of steps with no progress to consider stagnation
@@ -55,31 +59,34 @@ class SolitaireEnv(gymnasium.Env):
         self.observation_space = spaces.Box(
             low=-1, high=52, shape=(num_elements,), dtype=np.int32)
 
-        self.current_seed = None  # Initialize a variable to store the current seed
+        self.current_seed = None
         self.prev_state = {
             'foundation_count': [0, 0, 0, 0],
             'hidden_cards': set()
         }
-        # Define a dictionary for rewards
-        self.reward_values = self.config.get('reward_values', {})
+
         self.current_episode = 0
         self.total_episodes_count = 0
         self.current_step = 0
         self.move_count = 0
-        self.exploration_rate = None
+        self.model_stats = {}
 
     def reset(self, seed=2, return_info=False, options=None):
-        self.global_action_history = []
-
+        self.steps_since_progress = 0
         seed = next(number_gen)
-        config = {'cards_per_turn': 3, 'random_seed': seed}
+        self.current_seed = seed
+        config = self.config
+        print(f"Successful moves: {self.move_count}")
+        config.update({'random_seed': seed})
         self.game = Solitaire(config)
+        print("New game started.")
         observation = self.get_observation()
         info = {}  # You can add additional reset info if needed
         self.current_episode += 1
         self.current_step = 0
-        print(f"Successful moves: {self.move_count}")
+        
         self.move_count = 0
+
         # Make sure to return a tuple of (observation, info)
         return observation, info
 
@@ -92,76 +99,32 @@ class SolitaireEnv(gymnasium.Env):
         # Initialize reward
         reward = 0
 
-        # Get the game state before the action
-        prev_state = self.get_game_state()
-        
         # Execute the action
+
         if source_idx == 9:  # Deal next cards action
             self.game.deal_next_cards()
-            reward += self.reward_values['deal_cards']
+            self.steps_since_progress += 1
         else:
             source_stack = self.get_stack(source_idx)
             dest_stack = self.get_stack(dest_idx)
-            move_result = self.game.parse_parts(source_stack, dest_stack, num_cards=num_cards)
+            move_result, message, reward = self.game.execute_move(
+                source_stack, dest_stack, num_cards=num_cards)
             if move_result:
                 self.move_count += 1
-                reward += self.reward_values['move_success']
-                # Check for king moved to new empty stack
-                # Check if the destination is a tableau stack and the bottom card of the move is a king
-                if dest_stack.isdigit() and num_cards > 0:
-                    tableau_index = int(dest_stack) - 1
-                    stack = self.game.t_stack[tableau_index]
-                    if len(stack.cards) >= num_cards:
-                        bottom_card = stack.cards[-num_cards]
-                        if bottom_card.number == 13 and bottom_card.visible and num_cards == len(stack.cards):  # King
-                            reward += self.reward_values['move_king_to_empty_tableau']
+            if move_result and reward > 10:
+                self.steps_since_progress = 0
             else:
-                reward += self.reward_values['move_fail']
+                self.steps_since_progress += 1
 
-
-        # Get the updated game state
-        new_state = self.get_game_state()
-
-        # Check for progress in the foundation
-        if new_state['foundation_count'] > prev_state['foundation_count']:
-            reward += self.reward_values['add_to_foundation']
-        elif new_state['foundation_count'] < prev_state['foundation_count']:
-            reward += self.reward_values['remove_from_foundation']
-
-        # Check for revealing hidden cards
-        if len(new_state['hidden_cards']) < len(prev_state['hidden_cards']):
-            reward += self.reward_values['reveal_hidden']
-
-
-        # Update global action history
-        self.global_action_history.append(action)
-        if len(self.global_action_history) > self.config.get('max_global_history_length', 10):
-            self.global_action_history.pop(0)
-
-        # Calculate the frequency of the current action in the last x actions
-        recent_action_count = self.global_action_history.count(action)
-
-        # Apply penalty if action is repeated more than threshold and not the 'deal next cards' action
-        if recent_action_count > self.config.get('action_repeat_threshold', 2) and source_idx != 9:
-            penalty = self.reward_values['repeat_action'] * (recent_action_count ** 2)
-            reward += penalty
-
-        # Check for game completion and stagnation
+        # Check for game stagnation
         terminated = self.game.complete
-        if self.has_made_progress():
-            self.steps_since_progress = 0
-        else:
-            self.steps_since_progress += 1
-        if self.steps_since_progress >= self.config.get('stagnation_threshold', 1000):
+
+        if self.steps_since_progress >= self.config.get("env").get('stagnation_threshold', 1000):
             terminated = True
+            print(f"Current seed: {self.current_seed}")
+            self.game.show_score()
             self.game.show_cards()
-
-
-        # Reward for completing the game
-        if self.game.status():
-            reward += self.reward_values['game_complete']
-            print(f"Game completed in {self.current_step} steps")
-            terminated = True
+            print("Game stagnated. Terminating episode.")
 
         # Get the observation and additional info
         observation = self.get_observation()
@@ -170,7 +133,8 @@ class SolitaireEnv(gymnasium.Env):
         self.current_step += 1
         self.total_episodes_count += 1
 
-        self.log_action([self.total_episodes_count, self.current_episode, self.current_step, action, source_idx, dest_idx, num_cards, reward, terminated, self.exploration_rate])
+        self.log_action([self.total_episodes_count, self.current_episode, self.current_step, action,
+                        source_idx, dest_idx, num_cards, reward, terminated, self.model_stats.get('exploration_rate', 0)])
 
         return observation, reward, terminated, truncated, info
 
@@ -178,31 +142,6 @@ class SolitaireEnv(gymnasium.Env):
         with open(self.action_log_file, 'a', newline='') as file:
             writer = csv.writer(file)
             writer.writerow(log_data)
-
-
-    def has_made_progress(self):
-        # Get the current game state
-        current_state = self.get_game_state()
-
-        # Compare the current state with the previous state to determine progress
-        progress_made = current_state['foundation_count'] != self.prev_state['foundation_count'] \
-            or current_state['hidden_cards'] != self.prev_state['hidden_cards']
-
-        # Update previous state for the next comparison
-        self.prev_state = current_state
-
-        return progress_made
-
-    def get_game_state(self):
-        # Collect the current game state information
-        foundation_count = [len(stack.cards) for stack in self.game.foundation]
-        hidden_cards = set()
-        for stack in self.game.t_stack:
-            
-            for card in stack.cards:
-                if not card.visible:
-                    hidden_cards.add((card.suit, card.number))
-        return {'foundation_count': foundation_count, 'hidden_cards': hidden_cards}
 
     def render(self, mode='human'):
         if mode == 'human' or mode == 'ansi':
@@ -224,12 +163,13 @@ class SolitaireEnv(gymnasium.Env):
         # Initialize the observation array
         observation = np.zeros((13, self.max_cards_per_stack), dtype=np.int8)
 
-        for stack_idx, stack in enumerate(self.game.t_stack + self.game.foundation + [self.game.waste] + [self.game.next_cards]):
+        for stack_idx, stack in enumerate(self.game.t_stack + list(self.game.foundation.values()) + [self.game.waste] + [self.game.next_cards]):
             for card_idx in range(self.max_cards_per_stack):
                 if card_idx < len(stack.cards):
                     card = stack.cards[card_idx]
                     if card.visible:
-                        observation[stack_idx, card_idx] = self.encode_card(card)
+                        observation[stack_idx,
+                                    card_idx] = self.encode_card(card)
                     else:
                         observation[stack_idx, card_idx] = -1  # Hidden card
                 else:
@@ -247,11 +187,11 @@ class SolitaireEnv(gymnasium.Env):
     def decode_action(self, action):
         num_destinations = 8
 
-        #if action == self.action_space.n - 1:
+        # if action == self.action_space.n - 1:
         #    source_stack = 9
         #    destination_stack = 1
         #    num_cards = 1
-        #else:
+        # else:
         source_stack = action // (num_destinations * self.max_cards_per_move)
         action %= (num_destinations * self.max_cards_per_move)
         destination_stack = action // self.max_cards_per_move
